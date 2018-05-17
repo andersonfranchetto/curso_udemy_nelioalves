3b950008b0e492087a6890dddd63c8c04e292a2a
Anderson <anderson.franchetto@gmail.com>
2018-05-17 11:24:02 -0300

    Deletando uma Categoria com metodo DELETE. Tratando erro com o DataIntegrityException personalizado.

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -4,6 +4,8 @@ import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repositories.CategoriaRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.nelioalves.cursomc.services.exceptions.DataIntegrityException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,7 +28,16 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria categoria){
-        find(categoria.getId());
+        find(categoria.getId());//VERIFICA SE ID EXISTE.
         return repository.save(categoria);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            repository.delete(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria relacionada a um ou mais produtos.");
+        }
     }
 }

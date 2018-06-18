7133d879718fbc4819e2a25d27b76aad06a0d1c0
Anderson <anderson.franchetto@gmail.com>
2018-06-18 13:24:44 -0300

    Criando profile test

---
+++ src/main/java/com/nelioalves/cursomc/config/profile/TestConfig.java
@@ -0,0 +1,23 @@
+package com.nelioalves.cursomc.config.profile;
+
+import com.nelioalves.cursomc.services.profile.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}

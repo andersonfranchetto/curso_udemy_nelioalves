bfea23bae2a4533a5597c45f5a4940e0d27f2a73
Anderson <anderson.franchetto@gmail.com>
2018-06-18 14:23:26 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,17 +1,10 @@
 package com.nelioalves.cursomc;
 
-import com.nelioalves.cursomc.domain.*;
-import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
-import com.nelioalves.cursomc.domain.enums.TipoCliente;
-import com.nelioalves.cursomc.repositories.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
---
+++ src/main/java/com/nelioalves/cursomc/config/profile/DevConfig.java
@@ -0,0 +1,29 @@
+package com.nelioalves.cursomc.config.profile;
+
+import com.nelioalves.cursomc.services.profile.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if(!"create".equals(strategy)) return false;
+
+        dbService.instantiateTestDatabase();
+        return true;
+    }
+}
